d072ce5500b1163347f362aa1623b9a031795768
Michael Spencer <ms53150@gmail.com>
2023-03-06 21:47:49 -0500

    update readme

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
+import os
 
 app = Flask(__name__)
 app.app_context().push()
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("PASSWORD")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
